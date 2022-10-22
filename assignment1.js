31b68d114aa0e55d04724e2251115e6b07feb5b1
Saichand-1 <61727266+Saichand-1@users.noreply.github.com>
2022-10-22 15:00:14 +0530

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,39 @@
+//1. Write a program to find whether a given year is a leap year or not.
+// a = (year)=>
+// {
+//     if((year%4==0) && (year%100!=0 || year%400==0)){
+//         console.log("leap year")
+//     }
+//     else{
+//         console.log("not leap year")
+//     }
+// }
+
+// a(1900)
+// 2. Write a JavaScript program to convert temperatures to and from Celsius, Fahrenheit.
+// f_c = (f)=>{
+//     let val = (f-32)/9
+//     far = val*5
+//     console.log(far)
+// }
+
+// f_c(45)
+
+// c_f = (c) =>{
+//     let val2 = (c*1.8)+32
+//     console.log(val2)
+// }
+
+// c_f(60)
+
+//3. Write a program to find the factorial of a number.
+// fact = (x)=>{
+//     var facto = 1
+//     while(x>0){
+//         facto = facto*x
+//         x=x-1
+//     }
+//     return facto
+// }
+    
+// console.log(fact(5))
