c43b1017ef54b25f19a34deff3085b0ed458a09f
Saichand-1 <61727266+Saichand-1@users.noreply.github.com>
2022-10-22 19:09:21 +0530

    Add files via upload

---
+++ assign3.js
@@ -0,0 +1,51 @@
+// 1.Using for loops, write a Javascript program to output the following
+// c = 0
+// for(i=0;i<4;i++)
+// {
+//     for (j=0;j<=i;j++)
+//     {
+//         c+=1
+//         console.log(c," ")
+//     }
+//     console.log("\n")
+// }
+
+//2. Write a program to find whether a given number is armstrong number or
+// d = 153
+// let sum = 0
+// temp = d
+// while(d){
+//     rem = d%10
+//     sum  = sum+(rem*rem*rem)
+//     d=parseInt(d/10)
+// }
+// if(sum==temp){
+//     console.log("yes")
+// }
+// else{
+//     console.log("no")
+// }
+
+//3. a program to find whether a given number is special number or not-
+// d = 145
+// let sum = 0
+// temp = d
+// while(d){
+//     rem = d%10
+//     fact = 1
+//     temp1 = rem
+//     while(temp1>0)
+//     {
+//         fact = fact*temp1
+//         temp1 = temp1-1
+//     }
+//     sum=sum+fact
+//     d=parseInt(d/10)
+// }
+// if(sum==temp){
+//     console.log("yes")
+// }
+// else{
+//     console.log("no")
+// }
+ 
