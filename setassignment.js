e947f5e83f674f210ecf1eec15fd333b6334a7ae
Saichand-1 <61727266+Saichand-1@users.noreply.github.com>
2022-10-30 22:02:57 +0530

    Add files via upload

---
+++ setassignment.js
@@ -0,0 +1,24 @@
+//we can't add duplicate values in set object
+
+
+// let set = new Set()
+// set.add(1)
+// set.add(3)
+// set.add(1)
+// set.add(7)
+// set.add(6)
+// set.delete(6)
+// console.log(set)
+
+// let set = new Set()
+// function maxi(max,min,n)
+// {
+//     for(let i=0;i<n;i++)
+//     {
+//     set.add(Math.floor(Math.random()*(max-min+1)+min))
+//     }
+//     console.log(set)
+//     console.log(set.has(8))
+// }
+
+// maxi(10,0,4)
