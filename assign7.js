ead11576c737e0f13d1fff910ecd323a73e503c5
Saichand-1 <61727266+Saichand-1@users.noreply.github.com>
2022-11-01 19:32:12 +0530

    Add files via upload

---
+++ assign7.js
@@ -0,0 +1,102 @@
+
+// function q1(str)
+// {
+//     var str = str
+//     var  str1="" 
+//     if (str.length>4)
+//     {
+//         str1 = str1.concat(str.substring(0,4),"...")
+//     }
+//     else{
+//         str1= str
+//     }
+
+//     return str1
+// }
+
+// console.log(q1("sai"))
+
+// function q2(str)
+// {
+//     var str = str
+//     console.log(str.replace(" ",""))
+// }
+// q2("sai chand")
+
+// str  = "Hi boy"
+// str1 = str.split(" ")
+// str3 = str1.reverse().join(" ")
+// console.log(str3)
+
+
+// str = "apple"
+// console.log(str.replace("a","x"))  
+
+
+// split method is used to convert string to array
+
+//Match method is used to check the occurence
+
+
+//add \n to break a string to a newline in Javascript
+
+
+// function lowercasecheck(str)
+// {
+//     if(str.charCodeAt(0)>97 && str.charCodeAt(0)<122)
+//     {
+//         console.log("lower case")
+//     }
+//     else{
+//         console.log("Not lower case")
+//     }
+// }
+
+// lowercasecheck("Fuhfow")
+
+
+
+// function verdict(str){
+//     if(str.toLowerCase()=='yes')
+//     {
+//         console.log("correct verdict given")
+//     }
+// }
+
+// verdict("YEs")
+
+
+    // str1 = str.toUpperCase()
+    // console.log(str1)
+    
+
+    // s = str[0].toUpperCase()
+    // str2 = ""
+    // s1 = str2.concat(str[0].toUpperCase(),str.slice(1,str.length))
+    // console.log(s1)
+
+    // str2 = str.toLowerCase()
+
+    // str = "saichand"
+    // emp = ""
+    // hf = parseInt(str.length/2)
+    // str1 = str.slice(0,hf)
+    // str2 = str.slice(hf,str.length)
+    // swap = emp.concat(str2,str1)
+    // console.log(swap)
+
+    // str = "sssaaiiii"
+    // var dict = {}
+    // for(let i = 0;i<str.length;i++)
+    // {
+    //     if(dict[str[i]]){
+    //         dict[str[i]] = dict[str[i]]+1
+    //     }
+    //     else{
+    //         dict[str[i]] = 1
+    //     }
+    // }
+    // console.log(dict)
+
+    // str= "concat"
+    // console.log(str.split("").reverse().join(""))
